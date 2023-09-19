caa60f289a82e388ef6f16027bb79be7e53c6a96
Jack Zheng <260021@bjsmicschool.com>
2023-09-19 11:14:06 +0800

    Imported uvicorn added wrong user route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,6 +7,15 @@ app = FastAPI()
 def read_root():
     return {"Hello":"World!"}
 
+
 @app.get("/about")
-def read_root():
+def read_about():
     return {"Name":"Jack Zheng!"}
+
+
+@app.get("/user/{user_id}")
+def read_user(user_id: str):
+    return{"id": user_id}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",reload=True)
