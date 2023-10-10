e5b5e5b819c188925b64960553d5497c5e8e5991
Jack Zheng <260021@bjsmicschool.com>
2023-10-10 11:04:24 +0800

    Took notes on typing

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from fastapi import FastAPI
+
 import uvicorn
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World!"}
+users = [
+    {"name": "Jack", "grade": 10},
+    {"name": "Hudson", "grade": 11},
+    {"name": "Nolan", "grade": 10},
+]
 
+@app.get("/users/me")
+async def get_me():
+    return {"Me": "Jack"}
 
-@app.get("/about")
-def read_about():
-    return {"Name":"Jack Zheng!"}
+@app.get("/users/{user_id}")
+async def get_profile(user_id: int):
+    if user_id < 0 or user_id > len(users) - 1:
+        return {"message": "User not found"}
+    user = users[user_id]
+    return {"data": user}
 
+@app.get("/documents")
+async def get_document(name: str, age: int):
+    if name and age:
+        return {"name": name, "age": age}
+    return {"msg": "Got docs successfully"}
 
-@app.get("/user/{user_id}")
-def read_user(user_id: str):
-    return{"id": user_id}
-
+@app.get("/secret")
+async def get_secret():
+    return "Chemistry is shit."
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",reload=True)
-
-
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
