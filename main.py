7b2c83a83f42a155bea07fec6f3195951e330435
Jack Zheng <260021@bjsmicschool.com>
2023-10-24 11:11:36 +0800

    added static added good.jpg

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 from routers.coffee_taste import router_taste
 from routers.coffee_stock import router_stock
 from routers.coffee_ranking import router_ranking
 
 app = FastAPI()
 
+
 app.include_router(router_taste, prefix="/coffee_taste")
 app.include_router(router_stock, prefix="/coffee_stock")
 app.include_router(router_ranking, prefix="/coffee_ranking")
+
+app.mount("/static",StaticFiles(directory="static"))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True)
